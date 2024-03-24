1e156635ba676bc971cd58e6132851f9522aa5a7
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-03-24 18:44:52 +0530

    merge sort

---
+++ 4. merge sort.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void merge(int arr1[],int arr2[],int arr3[]){
+ int  i,j,k;
+  i=j=k=0;
+  while(i<5 && j<5){
+    if(arr1[i]<arr2[j]){
+      arr3[k++]=arr1[i++];
+   
+    }
+    else{
+      arr3[k++]=arr2[j++];
+      
+    }
+  }
+    while(i<5){
+      arr3[k++]=arr1[i++];
+    }
+    while(j<5){
+      arr3[k++]=arr2[j++];
+    }
+  
+
+  printf("sorted array is :");
+  for(int i=0; i<10; i++){
+    printf("%d\n",arr3[i]);
+  }
+}
+int main(){
+  int arr1[5],arr2[5],arr3[10];
+ 
+  int i;
+
+  printf("enter element of array for first array:");
+  for(i=0; i<5; i++){
+  scanf("%d",&arr1[i]);
+}
+   printf("enter element of array for first array:");
+    for(i=0; i<5; i++){
+    scanf("%d",&arr2[i]);
+  }
+  merge(arr1,arr2,arr3);
+  }
