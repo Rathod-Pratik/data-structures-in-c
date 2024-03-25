36c86649441e40e9e34aebe799e6bb2db1e97890
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-03-25 21:55:56 +0530

    all algorithm in program

--- 1. quick sort.c
+++ 1. quick sort.c
@@ -1,5 +1,41 @@
+/*
+algorithm for quick sort
+1. start
+
+2. Define a constant MAXSIZE to represent the maximum size of the array.
+
+3. Declare functions:
+a. sort(elements[], left, right): Sorts the elements in the array within the specified range.
+b. quicksort(elements[], maxsize): Initiates the quicksort algorithm on the entire array.
+
+4. Define the function quicksort(elements[], maxsize):
+a. Call the sort function with parameters elements[], 0, and maxsize - 1.
+
+5. Define the function sort(elements[], left, right):
+a. Declare variables: center, i, j, pivot, and temp.
+b. Set i to left, j to right, and calculate center as (left + right) / 2.
+c. Set pivot to elements[center].
+d. While i is less than or equal to j:
+i. Increment i until elements[i] is greater than or equal to pivot.
+ii. Decrement j until elements[j] is less than or equal to pivot.
+iii. If i is less than or equal to j:
+- Swap elements[i] and elements[j].
+- Increment i and decrement j.
+e. If left is less than j, recursively call sort with parameters elements[], left, and j.
+f. If i is less than right, recursively call sort with parameters elements[], i, and right.
+
+6. In the main function:
+a. Declare variables: elements[] and maxsize.
+b. Prompt the user to enter the number of elements.
+c. Read and store the value of maxsize.
+d. Prompt the user to enter values one by one and store them in the array elements[].
+e. Print "Array before sort:" followed by the elements of the array.
+f. Call quicksort function with parameters elements[] and maxsize.
+g. Print "Array after sort:" followed by the sorted elements of the array.
+
+7. End.
+*/
 #include <stdio.h>
-#include <stdlib.h>
 
 #define MAXSIZE 500
 
--- 2. bubble sort.c
+++ 2. bubble sort.c
@@ -1,3 +1,35 @@
+/*
+algoritm for bubble sort
+1. Start
+
+2. Define an array arr of size 5 to store the elements.
+
+3. Set n to 5, representing the number of elements in the array.
+
+4. Output "Enter elements of the array: ".
+
+5. Read and store 5 integers into the array arr.
+
+6. Initialize a variable count to 0 for the outer loop.
+
+7. Outer Loop: Repeat n - 1 times:
+    a. Increment count by 1.
+    b. Initialize a variable i to 0 for the inner loop.
+    c. Inner Loop: Repeat n - count - 1 times:
+        i. Compare arr[i] with arr[i + 1].
+        ii. If arr[i] is greater than arr[i + 1]:
+            - Swap arr[i] and arr[i + 1].
+    d. End of Inner Loop.
+    
+8. Output "Sorted array: ".
+
+9. Iterate through the sorted array:
+    a. Output each element of arr separated by a space.
+    
+10. Output a newline character to move to the next line.
+
+11. End.
+*/
 #include<stdio.h>
 int main() {
   int arr[5];
