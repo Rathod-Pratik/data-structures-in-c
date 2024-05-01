ab386b06e40087a79cd71babac56190d3b68405b
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-05-01 19:30:04 +0530

    Update 5. selection sort.c

--- 5. selection sort.c
+++ 5. selection sort.c
@@ -1,30 +1,27 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(void) {
-int n=5,i,k[5];
+int main() {
+
+int n=5,i,array[n],j,min_index,temp;
+
+  printf("enter element of array :");
   for(i=0; i<5; i++){
-    scanf("%d",&k[i]);
+    scanf("%d",&arra[i]);
   }
-  int j,min_index,temp;
 
-  printf("\n unsorted array :");
-  for(i=0; i<5;  i++){
-    printf("%d ",k[i]);
-  }
   for(i=0; i<n-1; i++){
     min_index=i;
     for(j=i+1; j<n; j++){
-      
       if(k[j]<k[min_index]){
         min_index=j;
-      }
+      }   
     } 
-      //swap element
-      if(min_index!=i){
-        temp=k[i];
-        k[i]=k[min_index];
-        k[min_index]=temp;
-      }
+    //swap element
+    if(min_index!=i){
+      temp=k[i];
+      k[i]=k[min_index];
+      k[min_index]=temp;
+    }
   }
   printf("\n sorted array :");
   for(i=0; i<5; i++){
