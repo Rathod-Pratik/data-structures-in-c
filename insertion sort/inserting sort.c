729152fee1df8fa90d6baa46d638ba954865e390
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-05-02 22:37:42 +0530

    make file

--- insertion sort/inserting sort.c
+++ insertion sort/inserting sort.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
-
 int main(void) {
-    int arr[10] = {1, 4, 7, 99, 84, 74, 54, 66, 12, 88};
+    int arr[10];
     int i, j, temp;
-
-    printf("Unsorted array: ");
+    printf("Enter element of array: ");
     for (i = 0; i < 10; i++) {
-        printf("%d\t", arr[i]);
+        scanf("%d", &arr[i]);
     }
     
     for (i = 1; i < 10; i++) {
         temp = arr[i];
         j = i - 1;
-        
         while (j >= 0 && arr[j] > temp) {
             arr[j + 1] = arr[j];
             j = j - 1;
